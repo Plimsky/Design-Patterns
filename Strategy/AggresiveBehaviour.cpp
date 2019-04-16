61cfbe6b7d02391c7602d81a3b4a866155a3cf32
Vincent Nicopolsky <vincent.nicopolsky@gmail.com>
2019-04-16 11:40:12 -0400

    Update project configuration

--- Strategy/AggresiveBehaviour.cpp
+++
@@ -1,7 +0,0 @@
-﻿#include "AggressiveBehaviour.h"
-
-void AggressiveBehaviour::Move() {}
-void AggressiveBehaviour::Attack(const Entity&) {}
-void AggressiveBehaviour::Jump() {}
-void AggressiveBehaviour::Crouch() {}
-void AggressiveBehaviour::Dodge() {}
