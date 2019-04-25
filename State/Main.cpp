4d998bf09ecd5e3d3e20a1f31928d9089846bd18
Vincent Nicopolsky <vincent.nicopolsky@gmail.com>
2019-04-25 15:50:14 -0400

    Add Diagram Class UML for State Pattern

--- State/Main.cpp
+++ State/Main.cpp
@@ -4,17 +4,17 @@
 
 int main()
 {
-	Player p1{};
-	int value;
+    Player p1{};
+    int value;
 
     while ((value = _getch()) != 27)
     {
-		if (value == 32)
-			p1.DoJump();
-		else if (value == 'a')
-			p1.Idle();
-		else if (value == 'e')
-			p1.DoAttack();
+        if (value == 32) // Spacebar
+            p1.DoJump();
+        else if (value == 'a')
+            p1.Idle();
+        else if (value == 'e')
+            p1.DoAttack();
     }
 
     return 0;
